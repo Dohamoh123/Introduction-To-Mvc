56ce21bb525ca1c2c4112fb4e0fc9176add3eb47
Doha AlMutlaq <Dohamutlaq123@gmail.com>
2021-02-02 19:08:13 +0200

    add mvc from scratch model controller view

---
+++ MVC From Scratch/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_From_Scratch.Models;
+
+namespace MVC_From_Scratch.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult DisplayCustomer()
+        {
+            var Customer = new Customer(1, "Doha", "Irbid", 24);
+            return View(Customer);
+        }
+
+       public ActionResult Create ()
+        {
+            var cust = new Customer();
+            return View(cust);
+        }
+    }
+}
---
+++ MVC From Scratch/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_From_Scratch.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public String Name { get; set; }
+        public String Address { get; set; }
+        public int Age { get; set; }
+
+        public Customer ()
+            {
+
+
+           }
+        public Customer(int id, string name, string address, int age)
+        {
+            ID = id;
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+    }
+}
